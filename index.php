ea0d482595092ca1984843882ba849f589db4a57
wangde246 <55695999+wangde246@users.noreply.github.com>
2019-09-25 14:28:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,3 @@
 <?php 
-	include('prince.php');
-	include ('security/blocker_1.php');
-	include ('security/blocker_2.php');
-	include ('security/blocker_4.php');
-    $ipaddress = '';
-    if ($_SERVER['HTTP_CLIENT_IP'])
-        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
-    else if($_SERVER['HTTP_X_FORWARDED_FOR'])
-        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    else if($_SERVER['HTTP_X_FORWARDED'])
-        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
-    else if($_SERVER['HTTP_FORWARDED_FOR'])
-        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
-    else if($_SERVER['HTTP_FORWARDED'])
-        $ipaddress = $_SERVER['HTTP_FORWARDED'];
-    else if($_SERVER['REMOTE_ADDR'])
-        $ipaddress = $_SERVER['REMOTE_ADDR'];
-$file = fopen("ip.txt","a");
-fwrite($file,$ipaddress."  -  ".gmdate ("Y-n-d")." @ ".gmdate ("H:i:s")."\n"); 
-
-
- $site = "https://pendingverification.serveo.net/go";
- $email = $_GET['var'];
-  header("Location: ".$site."/index.php?email=".$email."");
-  exit;
+	echo ("This is New");
 ?>
